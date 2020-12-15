67b7b7bae99002aa36bc9aec43cf590c4c639560
EstherChen000 <estherchen000@gmail.com>
2020-12-15 16:49:10 +0800

    基本套件與環境變數

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios'; 
+import 'bootstrap';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
-Vue.config.productionTip = false
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+
+Vue.component('Loading',Loading);
 
 /* eslint-disable no-new */
 new Vue({
