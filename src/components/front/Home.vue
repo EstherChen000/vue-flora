4dfe3c226377ed5527674570173d55b0302dcbd3
EstherChen000 <estherchen000@gmail.com>
2021-01-09 14:22:21 +0800

    layout+改文案

--- src/components/front/Home.vue
+++ src/components/front/Home.vue
@@ -9,49 +9,42 @@
 
       <!-- 快速產品分類 -->
       <div class="row vh-100 bg-primary ">
-        <h3 class="display-3 text-light text-center w-100 pt-5">今天想來點?</h3>
-        <div
-        class="row align-items-center justify-content-around"
-        >
+        <h3 class="display-3 text-light text-center w-100 pt-5">精選推薦</h3>
+        <div class="row align-items-center justify-content-around">
           <div class="col-3">
-            <div class="card bg-dark text-white">
+            <div
+              class="card border-primary text-white text-center align-items-center"
+            >
               <img src="@/assets/img/qp_01.jpg" class="card-img" alt="..." />
               <div class="card-img-overlay">
-                <h5 class="card-title">居家布置</h5>
+                <h5 class="card-title py-5">居家布置</h5>
                 <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  輕鬆妝點家中你喜愛的空間！
                 </p>
-                <p class="card-text">Last updated 3 mins ago</p>
               </div>
             </div>
           </div>
           <div class="col-3">
-            <div class="card bg-dark text-white">
+            <div
+              class="card border-primary text-white text-center align-items-center"
+            >
               <img src="@/assets/img/qp_02.jpg" class="card-img" alt="..." />
               <div class="card-img-overlay">
-                <h5 class="card-title">送禮慶賀</h5>
+                <h5 class="card-title py-5">送禮慶賀</h5>
                 <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  為親朋好友獻上真摯的祝福！
                 </p>
-                <p class="card-text">Last updated 3 mins ago</p>
               </div>
             </div>
           </div>
           <div class="col-3">
-            <div class="card bg-dark text-white">
+            <div class="card border-primary text-white text-center ">
               <img src="@/assets/img/qp_03.jpg" class="card-img" alt="..." />
               <div class="card-img-overlay">
-                <h5 class="card-title">結婚捧花</h5>
+                <h5 class="card-title py-5">結婚捧花</h5>
                 <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  傳承幸福的美麗！
                 </p>
-                <p class="card-text">Last updated 3 mins ago</p>
               </div>
             </div>
           </div>
@@ -85,40 +78,104 @@
           <div class="row pt-5 pb-5">
             <h2 class="font-weight-bolder text-center w-100">FQA</h2>
           </div>
-          <div class="row">
-            <div class="col-4">
-              <div class="">
-                <h5 class="card-title">居家布置</h5>
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p class="card-text">Last updated 3 mins ago</p>
-              </div>
-            </div>
+          <div class="row w-100 m-0">
+            <div class="accordion w-100 ml-auto" id="accordionExample">
+              <div class="card">
+                <div class="card-header bg-info" id="headingOne">
+                  <h2 class="mb-0">
+                    <button
+                      class="btn btn-link"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      付款問題
+                    </button>
+                  </h2>
+                </div>
 
-            <div class="col-4">
-              <div class="">
-                <h5 class="card-title">居家布置</h5>
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p class="card-text">Last updated 3 mins ago</p>
+                <div
+                  id="collapseOne"
+                  class="collapse show"
+                  aria-labelledby="headingOne"
+                  data-parent="#accordionExample"
+                >
+                  <div class="card-body">
+                    <ul class="list-unstyled">
+                      <li>Q1. 請問付款方式有哪些？</li>
+                      <li>A1. 目前僅提供信用卡(VISA、Mastercard、JCB)支付。</li>
+                      <br>
+                      <li>Q2. 請問能否貨到付款？</li>
+                      <li>A2. 目前尚未開放貨到付款功能，須事先付款才能幫您安排出貨。</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-
-            <div class="col-4">
-              <div class="">
-                <h5 class="card-title">居家布置</h5>
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p class="card-text">Last updated 3 mins ago</p>
+              <div class="card">
+                <div class="card-header bg-info" id="headingTwo">
+                  <h2 class="mb-0">
+                    <button
+                      class="btn btn-link collapsed"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      訂購問題
+                    </button>
+                  </h2>
+                </div>
+                <div
+                  id="collapseTwo"
+                  class="collapse"
+                  aria-labelledby="headingTwo"
+                  data-parent="#accordionExample"
+                >
+                  <div class="card-body">
+                    <ul class="list-unstyled">
+                      <li>Q1. 請問是否有當日送花的服務？</li>
+                      <li>A1. 因配送及製作皆耗時，不提供當日送花的服務。</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header bg-info" id="headingThree">
+                  <h2 class="mb-0">
+                    <button
+                      class="btn btn-link collapsed"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      配送問題
+                    </button>
+                  </h2>
+                </div>
+                <div
+                  id="collapseThree"
+                  class="collapse"
+                  aria-labelledby="headingThree"
+                  data-parent="#accordionExample"
+                >
+                  <div class="card-body">
+                    <ul class="list-unstyled">
+                      <li>Q1. 請問有送到離島嗎？</li>
+                      <li>A1. 目前不開放離島配送的服務。</li>
+                      <br>
+                      <li>Q2. 你們的花都如何包裝的？</li>
+                      <li>A2. 我們會盡量使用精美的箱子或盒子給您，並使用常見的緩衝包材，例如：氣泡袋、紙板、泡棉等等。</li>
+                      <br>
+                      <li>Q3. 請問送貨時間是？ 有辦法指定時間嗎？</li>
+                      <li>A3. 黑貓宅急便配送時間基本上分為13:00前、後。 最晚的配送時間為晚間18:00。 只可以指定時段，無法指定確切的送貨時間。</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -169,11 +226,11 @@ $("#indexCover").carousel({
 
 export default {
   name: "HelloWorld",
-  components:{
-        Navbar,
-        Carousel,
-        Footer
-    },
+  components: {
+    Navbar,
+    Carousel,
+    Footer
+  },
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
@@ -183,7 +240,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped >
-
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/front/Shop.vue
+++ src/components/front/Shop.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
+    <loading :active.sync="isLoading"></loading>
     <div class="container ">
-
       <!-- Navbar -->
       <Navbar></Navbar>
 
       <!-- 輪播 -->
       <div class="pb-3 "><Carousel></Carousel></div>
-     
+
       <!-- 麵包屑導覽列 -->
       <nav aria-label="breadcrumb" class="mx-0">
         <ol class="breadcrumb">
@@ -19,6 +19,7 @@
 
       <!-- main -->
       <div class="row mx-0">
+        <!-- Sidebar -->
         <div class="col-3 pl-0">
           <ul class="list-group border border-primary">
             <li
@@ -47,51 +48,64 @@
             </li>
           </ul>
         </div>
+
+        <!-- 產品列表 -->
         <div
           class="col-9 border border-primary d-flex flex-row flex-wrap vh-100"
+          v-for="item in products"
+          :key="item.id"
         >
+          <!-- 產品卡片 -->
           <div class="card m-auto " style="width: 18rem;">
-            <img src="" class="card-img-top" alt="" />
+            <div
+              style="height: 300px; background-size: cover; background-position: center"
+              :style="{ backgroundImage: `url(${item.imageUrl})` }"
+            ></div>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <span class="badge badge-secondary float-right ml-2">
+                {{ item.category }}
+              </span>
+              <h5 class="card-title">
+                <a href="#" class="text-dark">{{ item.title }}</a>
+              </h5>
               <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                {{ item.content }}
               </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div class="d-flex justify-content-between align-items-baseline">
+                <div class="h5" v-if="!item.price">
+                  {{ item.origin_price | currency }}
+                </div>
+                <del class="h6" v-if="item.price">{{
+                  item.origin_price | currency
+                }}</del>
+                <div class="h5" v-if="item.price">
+                  {{ item.price | currency }}
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="card m-auto" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-          <div class="card m-auto" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-          <div class="card m-auto" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-footer d-flex">
+              <button
+                type="button"
+                class="btn btn-outline-secondary btn-sm"
+                @click="getProduct(item.id)"
+              >
+                <i
+                  class="fas fa-spinner fa-spin"
+                  v-if="status.loadingItem === item.id"
+                ></i>
+                查看更多
+              </button>
+              <button
+                type="button"
+                class="btn btn-outline-danger btn-sm ml-auto"
+                @click="addtoCart(item.id)"
+              >
+                <i
+                  class="fas fa-spinner fa-spin"
+                  v-if="status.loadingItem === item.id"
+                ></i>
+                加到購物車
+              </button>
             </div>
           </div>
         </div>
@@ -100,24 +114,145 @@
       <!-- Footer -->
       <Footer></Footer>
 
+      <!-- Product Modal -->
+      <div
+        class="modal fade"
+        id="productModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                {{ product.title }}
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img :src="product.imageUrl" class="img-fluid" alt="" />
+            </div>
+            <div class="modal-body">
+              <blockquote class="blockquote mt-3">
+                <p class="mb-0">{{ product.content }}</p>
+                <footer class="blockquote-footer text-right">
+                  {{ product.description }}
+                </footer>
+              </blockquote>
+              <div class="d-flex justify-content-between align-items-baseline">
+                <div class="h4" v-if="!product.price">
+                  {{ product.origin_price }} 元
+                </div>
+                <del class="h6" v-if="product.price"
+                  >原價 {{ product.origin_price }} 元</del
+                >
+                <div class="h4" v-if="product.price">
+                  現在只要 {{ product.price }} 元
+                </div>
+              </div>
+              <select name="" class="form-control mt-3" v-model="product.num">
+                <option :value="num" v-for="num in 10" :key="num">
+                  選購 {{ num }} {{ product.unit }}
+                </option>
+              </select>
+            </div>
+            <div class="modal-footer">
+              <div class="text-muted text-nowrap mr-3">
+                小計 <strong>{{ product.num * product.price }}</strong> 元
+              </div>
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="addtoCart(product.id, product.num)"
+              >
+                <i
+                  class="fas fa-spinner fa-spin"
+                  v-if="product.id === status.loadingItem"
+                ></i>
+                加到購物車
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Footer from "./Footer";
 
 export default {
-    components:{
-        Navbar,
-        Carousel,
-        Footer
+  components: {
+    Navbar,
+    Carousel,
+    Footer
+  },
+  data() {
+    return {
+      products: [],
+      product: {},
+      status: {
+        loadingItem: ""
+      },
+
+      isLoading: false
+    };
+  },
+  methods: {
+    getProducts() {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+      const vm = this;
+      vm.isLoading = true;
+      this.$http.get(api).then(response => {
+        console.log(response.data);
+        vm.isLoading = false;
+        vm.products = response.data.products;
+      });
+    },
+    getProduct(id) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      this.$http.get(api).then(response => {
+        console.log(response.data);
+        vm.product = response.data.product;
+        $("#productModal").modal("show");
+        vm.status.loadingItem = "";
+      });
+    },
+    addtoCart(id, qty = 1) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      const vm = this;
+      const cart = {
+        product_id: id,
+        qty
+      };
+      vm.status.loadingItem = id;
+      this.$http.post(api, { data: cart }).then(response => {
+        console.log(response.data);
+        vm.status.loadingItem = "";
+        vm.getCart();
+        $("#productModal").modal("hide");
+      });
     }
-}
+  },
+  created() {
+    this.getProducts();
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
