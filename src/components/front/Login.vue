18c0226c625925ec3880ba9f96db90cbf63bcf2d
EstherChen000 <estherchen000@gmail.com>
2021-01-22 22:47:22 +0800

    Login 優化

--- src/components/front/Login.vue
+++ src/components/front/Login.vue
@@ -1,82 +1,128 @@
 <template>
   <div>
-    <div class="login-wrap">
-      <div class="login-html">
-        <input
-          id="tab-1"
-          type="radio"
-          name="tab"
-          class="sign-in"
-          checked
-        /><label for="tab-1" class="tab">登入</label>
-        <input id="tab-2" type="radio" name="tab" class="sign-up" /><label
-          for="tab-2"
-          class="tab"
-          >註冊</label
-        >
-        <div class="login-form">
-          <div class="sign-in-htm">
-            <div class="form-group">
-              <label for="user" class="label">Username</label>
-              <input id="user" type="text" v-model="user.username" class=" form-control rounded-pill" />
-            </div>
-            <div class="form-group">
-              <label for="pass" class="label">Password</label>
-              <input
-                id="pass"
-                type="password"
-                class="form-control "
-                data-type="password"
-                v-model="user.password"
-              />
-            </div>
-            <div class="group">
-              <input id="check" type="checkbox" class="check" checked />
-              <label for="check"
-                ><span class="icon"></span> 保持登入狀態 </label
-              >
-            </div>
-            <div class="group">
-              <input type="submit" class="btn btn-primary btn-lg " @click.prevent="signin" value="Sign In" />
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-              <a href="#forgot">Forgot Password?</a>
+    <div class="bg-light w-100 col-lg-4 col-md-6 col-sm-12 m-auto shadow-lg border border-primary">
+      <div class="p-sm-3 p-md-5">
+        <ul class="nav nav-tabs nav-pills nav-fill" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              id="signin-tab"
+              data-toggle="tab"
+              href="#signin"
+              role="tab"
+              aria-controls="signin"
+              aria-selected="true"
+              >登入</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="signup-tab"
+              data-toggle="tab"
+              href="#signup"
+              role="tab"
+              aria-controls="signup"
+              aria-selected="false"
+              >註冊</a
+            >
+          </li>
+        </ul>
+        <div class="tab-content text-primary" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="signin"
+            role="tabpanel"
+            aria-labelledby="signin-tab"
+          >
+            <div class="sign-in-htm pt-3">
+              <div class="form-group">
+                <label for="user" class="label">帳戶</label>
+                <input
+                  id="user"
+                  type="text"
+                  v-model="user.username"
+                  class=" form-control"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pass" class="label">密碼</label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="form-control"
+                  data-type="password"
+                  v-model="user.password"
+                />
+              </div>
+              <div class="form-group form-check">
+                <input id="check" type="checkbox" class="form-check-input" checked />
+                <label for="check"
+                  >保持登入狀態
+                </label>
+              </div>
+              <div class="form-group">
+                <input
+                  type="submit"
+                  class="btn btn-primary btn-lg btn-block"
+                  @click.prevent="signin"
+                  value="登入"
+                />
+              </div>
+              <hr>
+              <div class="font-weight-light text-center">
+                <label for="tab-1">&copy;Flora2021</label>
+              </div>
             </div>
           </div>
-          <div class="sign-up-htm">
-            <div class="group">
-              <label for="user" class="label">Username</label>
-              <input id="user" type="text" class="form-control rounded-pill" />
-            </div>
-            <div class="group">
-              <label for="pass" class="label">Password</label>
-              <input
-                id="pass"
-                type="password"
-                class="form-control rounded-pill"
-                data-type="password"
-              />
-            </div>
-            <div class="group">
-              <label for="pass" class="label">Repeat Password</label>
-              <input
-                id="pass"
-                type="password"
-                class="form-control rounded-pill"
-                data-type="password"
-              />
-            </div>
-            <div class="group">
-              <label for="pass" class="label">Email Address</label>
-              <input id="pass" type="text" class="form-control rounded-pill" />
-            </div>
-            <div class="group">
-              <input type="submit" class="button" value="Sign Up" />
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-              <label for="tab-1">Already Member?</label>
+          <div
+            class="tab-pane fade"
+            id="signup"
+            role="tabpanel"
+            aria-labelledby="signup-tab"
+          >
+            <div class="sign-up-htm pt-3">
+              <div class="form-group">
+                <label for="user" class="label">帳戶</label>
+                <input
+                  id="user"
+                  type="text"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pass" class="label">密碼</label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="form-control"
+                  data-type="password"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pass" class="label">再次輸入密碼</label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="form-control"
+                  data-type="password"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pass" class="label">電子信箱</label>
+                <input
+                  id="pass"
+                  type="text"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group pt-md-3">
+                <input type="submit" class="btn btn-primary btn-lg btn-block" value="註冊" />
+              </div>
+              <hr>
+              <div class="font-weight-light text-center">
+                <label for="tab-1">&copy;Flora2021</label>
+              </div>
             </div>
           </div>
         </div>
@@ -91,8 +137,8 @@ export default {
   data() {
     return {
       user: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
       }
     };
   },
@@ -100,191 +146,20 @@ export default {
     signin() {
       const api = `${process.env.APIPATH}/admin/signin`;
       const vm = this;
-      this.$http.post(api, vm.user).then((response) => {
+      this.$http.post(api, vm.user).then(response => {
         console.log(response.data);
         if (response.data.success) {
           const token = response.data.token;
           const expired = response.data.expired;
           document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-          vm.$router.push('/admin/products');
+          vm.$router.push("/admin/products");
         }
       });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-body {
-  margin: 0;
-  color: #6a6f8c;
-  background: #c8c8c8;
-  font: 600 16px/18px "Open Sans", sans-serif;
-}
-*,
-:after,
-:before {
-  box-sizing: border-box;
-}
-.clearfix:after,
-.clearfix:before {
-  content: "";
-  display: table;
-}
-.clearfix:after {
-  clear: both;
-  display: block;
-}
-a {
-  color: inherit;
-  text-decoration: none;
-}
 
-.login-wrap {
-  width: 100%;
-  margin: auto;
-  max-width: 525px;
-  min-height: 670px;
-  position: relative;
-  background: url(https://raw.githubusercontent.com/khadkamhn/day-01-login-form/master/img/bg.jpg)
-    no-repeat center;
-  box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24),
-    0 17px 50px 0 rgba(0, 0, 0, 0.19);
-}
-.login-html {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  padding: 90px 70px 50px 70px;
-  background: rgba(40, 57, 101, 0.9);
-}
-.login-html .sign-in-htm,
-.login-html .sign-up-htm {
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  position: absolute;
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
-  transition: all 0.4s linear;
-}
-.login-html .sign-in,
-.login-html .sign-up,
-.login-form .group .check {
-  display: none;
-}
-.login-html .tab,
-.login-form .group .label,
-.login-form .group .button {
-  text-transform: uppercase;
-}
-.login-html .tab {
-  font-size: 22px;
-  margin-right: 15px;
-  padding-bottom: 5px;
-  margin: 0 15px 10px 0;
-  display: inline-block;
-  border-bottom: 2px solid transparent;
-}
-.login-html .sign-in:checked + .tab,
-.login-html .sign-up:checked + .tab {
-  color: #fff;
-  border-color: #1161ee;
-}
-.login-form {
-  min-height: 345px;
-  position: relative;
-  perspective: 1000px;
-  transform-style: preserve-3d;
-}
-.login-form .group {
-  margin-bottom: 15px;
-}
-.login-form .group .label,
-.login-form .group .input,
-.login-form .group .button {
-  width: 100%;
-  color: #fff;
-  display: block;
-}
-/* .login-form .group .input,
-.login-form .group .button {
-  border: none;
-  padding: 15px 20px;
-  border-radius: 25px;
-  background: rgba(255, 255, 255, 0.1);
-} */
-/* .login-form .group input[data-type="password"] {
-  text-security: circle;
-  -webkit-text-security: circle;
-} */
-.login-form .group .label {
-  color: #aaa;
-  font-size: 12px;
-}
-/* .login-form .group .button {
-  background: #1161ee;
-} */
-.login-form .group label .icon {
-  width: 15px;
-  height: 15px;
-  border-radius: 2px;
-  position: relative;
-  display: inline-block;
-  background: rgba(255, 255, 255, 0.1);
-}
-.login-form .group label .icon:before,
-.login-form .group label .icon:after {
-  content: "";
-  width: 10px;
-  height: 2px;
-  background: #fff;
-  position: absolute;
-  transition: all 0.2s ease-in-out 0s;
-}
-.login-form .group label .icon:before {
-  left: 3px;
-  width: 5px;
-  bottom: 6px;
-  transform: scale(0) rotate(0);
-}
-.login-form .group label .icon:after {
-  top: 6px;
-  right: 0;
-  transform: scale(0) rotate(0);
-}
-.login-form .group .check:checked + label {
-  color: #fff;
-}
-.login-form .group .check:checked + label .icon {
-  background: #1161ee;
-}
-.login-form .group .check:checked + label .icon:before {
-  transform: scale(1) rotate(45deg);
-}
-.login-form .group .check:checked + label .icon:after {
-  transform: scale(1) rotate(-45deg);
-}
-.login-html
-  .sign-in:checked
-  + .tab
-  + .sign-up
-  + .tab
-  + .login-form
-  .sign-in-htm {
-  transform: rotate(0);
-}
-.login-html .sign-up:checked + .tab + .login-form .sign-up-htm {
-  transform: rotate(0);
-}
-
-.hr {
-  height: 2px;
-  margin: 60px 0 50px 0;
-  background: rgba(255, 255, 255, 0.2);
-}
-.foot-lnk {
-  text-align: center;
-}
 </style>
