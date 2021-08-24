1a2ba8c792df4889f7ffa5116546d218456cc35d
EstherChen000 <estherchen000@gmail.com>
2021-08-24 22:11:18 +0800

    edit: 購物車刪除 & 畫面調整

--- src/components/front/pages/CartOrder.vue
+++ src/components/front/pages/CartOrder.vue
@@ -40,8 +40,18 @@
                       <i class="fas fa-minus"></i>
                     </button>
                   </div>
-                  <label for="button" class="form-control text-center" v-if="item.qty == item.qtyN">{{item.qtyN}}</label>
-                  <label for="button" class="form-control text-center" v-if="item.qty !== item.qtyN">{{item.qty}}</label>
+                  <label
+                    for="button"
+                    class="form-control text-center"
+                    v-if="item.qty == item.qtyN"
+                    >{{ item.qtyN }}</label
+                  >
+                  <label
+                    for="button"
+                    class="form-control text-center"
+                    v-if="item.qty !== item.qtyN"
+                    >{{ item.qty }}</label
+                  >
                   <div class="input-group-append">
                     <button
                       class="btn btn-outline-primary"
@@ -52,18 +62,16 @@
                     </button>
                   </div>
                 </div>
-                <div class="h6">
-                  優惠價 NT.{{ item.price | currency }}
-                </div>
+                <div class="h6">優惠價 NT.{{ item.price | currency }}</div>
                 <div class="h5 text-danger">
-                  小計 NT.{{ item.price * item.qty | currency }}
+                  小計 NT.{{ (item.price * item.qty) | currency }}
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="input-group mt-3 input-group-sm">
+        <div class="input-group mt-3 input-group-sm" v-if="!isCoupon">
           <input
             type="text"
             class="form-control"
@@ -80,7 +88,7 @@
             </button>
           </div>
         </div>
-        <div>{{codeValidate}}</div>
+        <div class="mt-1" :class="{ 'text-danger' : isWarrning}">{{ codeValidate }}</div>
       </div>
       <div class="col-md-5 col-11">
         <div class="card mt-2">
@@ -90,10 +98,7 @@
           <div class="card-body">
             <div class="text-right py-3">
               <div class="h6 ">小計 NT.{{ total | currency }}</div>
-              <div
-                v-if="finalTotal !== total"
-                class="h6 text-info"
-              >
+              <div v-if="finalTotal !== total" class="h6 text-info">
                 折扣價 NT.{{ finalTotal | currency }}
               </div>
               <div class="h6 ">運費 NT.0</div>
@@ -102,12 +107,12 @@
                 總計 NT.{{ finalTotal | currency }}
               </div>
             </div>
-            <router-link
+            <button
               class="btn btn-primary float-right"
               v-show="isCart === true"
-              to="/cart/cart_checkout"
+              @click="addCartAPIandCoupon"
               >下一步
-            </router-link>
+            </button>
           </div>
         </div>
       </div>
@@ -121,171 +126,88 @@ export default {
       status: {
         loadingItem: ""
       },
-      cartApi: {},
       isLoading: false,
       coupon_code: "",
       isCart: false,
       cartStorage: JSON.parse(localStorage.getItem("cartList")) || [],
-      cart:[],
+      cart: [],
       finalTotal: 0,
       total: 0,
-      discount:1,
-      coupon:[
-      {
-        title:"振興特優價",
-        code:"3jvpd9",
-        percent:0.85,
-        expiry:[2030,9,31]
-      },{
-        title:"新會員特惠",
-        code:"newmember",
-        percent:0.8,
-        expiry:[2030,11,31]
-      }],
+      discount: 1,
+      coupon: [
+        {
+          title: "振興特優價",
+          code: "3jvpd9",
+          percent: 0.85,
+          expiry: [2030, 9, 31]
+        },
+        {
+          title: "新會員特惠",
+          code: "newmember",
+          percent: 0.8,
+          expiry: [2030, 11, 31]
+        }
+      ],
       isCoupon: false,
-      off:0,
-      // cartApi:{}
+      isWarrning: false,
     };
   },
   methods: {
-    getCartAPI() {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
-      const vm = this;
-      let cList = [];
-      vm.isLoading = true;
-      //為什麼api上山不掉?
-      // const api2 = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/-MeEZNcP7thaSYcfZHsS`;
-      // vm.$http.delete(api2).then((res) => {
-      //   console.log("remove api")
-      //   console.log(res)
-      // });
-      vm.$http.get(api).then(response => {
-        vm.cartApi = response.data.data;
-        console.log("getCartAPI's res:", response);
-        vm.isLoading = false;
-        if (vm.cartApi.carts.length !== 0) {
-          vm.isCart = true;
-        } else {
-          vm.isCart = false;
-        }
-        vm.cartApi.carts.forEach(item => {
-          const cartContent = {
-            product_id: item.product_id,
-            qty: item.qty
-          };
-          console.log("forEach中的cartContent", cartContent);
-          cList.push(cartContent);
-          console.log("forEach中的cList", cList);
-        });
-        console.log("cList", cList);
-        vm.cartStorage = cList;
-        localStorage.removeItem("cartList");
-        localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-      });
-    },
     removeCartItem(id) {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
       const vm = this;
       let cList = [];
+      let keep = [];
+      let sum = 0;
       vm.isLoading = true;
-      // vm.cartStorage.forEach((item, index) => {
-      //   if(item.product_id = id){
-      //     vm.cartStorage.splice(index, 1);
-      //   }
-      // });
-      vm.$http.delete(api).then(res => {
-        //刪除Api上資料後再次取得並寫入localStage
-        vm.getCartAPI();
-        console.log("removeCartItem's res:", res);
-        vm.cartApi.carts.forEach(item => {
+      vm.cart.forEach(e => {
+        if(e.id !== id){
           const cartContent = {
-            product_id: item.product_id,
-            qty: item.qty
+            product_id: e.id,
+            qty: e.qty,
+            coupon: vm.coupon_code,
+            off: vm.discount
           };
-          console.log("forEach中的cartContent", cartContent);
-          cList.push(cartContent);
-          console.log("forEach中的cList", cList);
-        });
-        console.log("cList", cList);
-        vm.cartStorage = cList;
-        localStorage.removeItem("cartList");
-        localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-        vm.isLoading = false;
-      });
-      // localStorage.removeItem("cartList");
-      // localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-      // vm.storageToCart();
-    },
-    addCouponCode() {
-      // const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/coupon`;
-      // const vm = this;
-      // const coupon = {
-      //   code: vm.coupon_code
-      // };
-      // vm.isLoading = true;
-      // vm.$http.post(api, { data: coupon }).then(response => {
-      //   vm.getCartAPI();
-      //   vm.isLoading = false;
-      // });
-      // ---
-      const vm = this;
-      vm.coupon.forEach(e => {
-        if( vm.coupon_code.trim() === e.code && vm.isCoupon === false){
-          vm.off = e.percent
+          cList.push(cartContent)
+          keep.push(e)
         }
       })
-      if(vm.isCoupon === false){
-        vm.total = vm.total * vm.off
-        vm.finalTotal = vm.finalTotal * vm.off
-        vm.cart.forEach(e => {
-          e.price = e.price * vm.off
-        })
-        vm.isCoupon = true;
-      }
-      
-      // console.log(vm.finalTotal * vm.coupon[0].percent)
-      console.log(off)
-    },
-    addtoStorage(data, qty = 1) {
-      const vm = this;
-      const cIdList = [];
-      vm.cartStorage.forEach(item => {
-        cIdList.push(item.product_id);
+      vm.cart = keep;
+      vm.cartStorage = cList;
+      localStorage.removeItem("cartList");
+      localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
+      vm.isLoading = false;
+      keep.length === 0 ? vm.isCart = false : true
+      vm.cart.forEach(item => {
+        sum += item.price * item.qty;
       });
-      if (!cIdList.includes(data.id)) {
-        const cartContent = {
-          product_id: data.id,
-          qty: qty,
-          name: data.title,
-          origin_price: data.origin_price,
-          price: data.price
-        };
-        vm.cartStorage.push(cartContent);
-        // 重新寫入 localStorage
-        localStorage.removeItem("cartList");
-        localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-        vm.$bus.$emit("message:push", "已加入購物車", "success");
-      } else {
-        let cache = {}; // 產品暫存處
-        vm.cartStorage.forEach((item, index) => {
-          // 只找相同的產品內容
-          if (item.product_id === data.id) {
-            cache = {
-              product_id: data.id,
-              qty: (item.qty += qty), // 產品當前數量+新增數量
-              name: data.title,
-              origin_price: data.origin_price,
-              price: data.price
-            };
-            // 移除現有 localStorage 購物車的資料，否則 localStorage 會重複加入
-            vm.cartStorage.splice(index, 1);
-          }
+      vm.total = sum;
+      vm.finalTotal = sum * vm.discount;
+    },
+    addCouponCode() {
+      const vm = this;
+      vm.coupon.forEach(e => {
+        if (vm.coupon_code.trim() === e.code && vm.isCoupon === false) {
+          vm.discount = e.percent;
+        }
+      });
+      if (vm.isCoupon === false) {
+        let cache = {};
+        vm.total = vm.total * vm.discount;
+        vm.finalTotal = vm.finalTotal * vm.discount;
+        vm.cart.forEach(e => {
+          e.price = e.price * vm.discount;
+          cache = {
+            product_id: e.id,
+            qty: e.qty,
+            coupon: vm.coupon_code,
+            off: vm.discount
+          };
         });
+        vm.cartStorage = [];
         vm.cartStorage.push(cache);
-        // 重新寫入 localStorage
         localStorage.removeItem("cartList");
         localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-        vm.$bus.$emit("message:push", "已加入購物車", "success");
+        vm.isCoupon = true;
       }
     },
     storageToCart() {
@@ -294,26 +216,30 @@ export default {
       let cache = [];
       vm.$http.get(api).then(response => {
         cache = response.data.products;
-        if (localStorage.getItem("cartList") === null) {
+        if (vm.cartStorage.length === 0) {
           vm.isCart = false;
         } else {
           // 先取出localStorage中的ID在做篩選
           const id = [];
           let sum = 0;
+          vm.isCart = true;
           vm.cartStorage.forEach(item => {
             id.push(item.product_id);
+            if (item.coupon) {
+              vm.isCoupon = true;
+              vm.discount = item.off;
+              vm.coupon_code = item.coupon;
+            }
           });
-          //頭痛中
-          let test = [];
+          let keep = [];
           cache.forEach(item => {
             id.forEach(e => {
               if (item.id === e) {
-                test.push(item);
+                keep.push(item);
               }
             });
           });
-          vm.cart = test;
-          // console.log(test)
+          vm.cart = keep;
           vm.cart.forEach(item => {
             vm.cartStorage.forEach(e => {
               if (item.id === e.product_id) {
@@ -322,17 +248,20 @@ export default {
             });
             sum += item.price * item.qty;
           });
-          vm.isCart = true;
           vm.total = sum;
           vm.finalTotal = sum * vm.discount;
-          // console.log(cache);
         }
       });
     },
-    addtoCartAPI() {
+    addCartAPIandCoupon() {
       const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      const apiCoupon = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/coupon`;
       const vm = this;
       let cart = {};
+      const coupon = {
+        code: vm.coupon_code
+      };
+      vm.isLoading = true;
       vm.cartStorage.forEach(item => {
         cart = {
           product_id: item.product_id,
@@ -343,85 +272,73 @@ export default {
           console.log(response);
           console.log(cart);
         });
-        // console.log(cart);
+      });
+      vm.$http.post(apiCoupon, { data: coupon }).then(response => {
+        vm.isLoading = false;
+        localStorage.removeItem("cartList");
+        vm.$router.push(`/cart/cart_checkout`);
       });
     },
     productQty(act, id) {
       const vm = this;
       let cList = [];
-      if(act === 'add'){
+      if (act === "add") {
         vm.cart.forEach((e, key) => {
-          if(e.id === id){
+          if (e.id === id) {
             e.qty += 1;
-            // console.log(e.title,e.qty,vm.cart[key].qty);
             vm.$bus.$emit("message:push", "商品數目已變更", "success");
-            vm.$set(vm.cart[key], 'qtyN', e.qty)
+            vm.$set(vm.cart[key], "qtyN", e.qty);
           }
           const cartContent = {
             product_id: e.id,
             qty: e.qty
           };
           cList.push(cartContent);
-        })
-      }else if(act === 'reduce'){
+        });
+      } else if (act === "reduce") {
         vm.cart.forEach((e, key) => {
-          if(e.id === id){
+          if (e.id === id) {
             e.qty -= 1;
-            // console.log(e.title,e.qty,vm.cart[key].qty);
             vm.$bus.$emit("message:push", "商品數目已變更", "success");
-            vm.$set(vm.cart[key], 'qtyN', e.qty)
+            vm.$set(vm.cart[key], "qtyN", e.qty);
           }
           const cartContent = {
             product_id: e.id,
             qty: e.qty
           };
           cList.push(cartContent);
-        })
+        });
       }
       vm.cartStorage = cList;
       localStorage.removeItem("cartList");
       localStorage.setItem("cartList", JSON.stringify(vm.cartStorage));
-    },
-    sumTotal() {
-      const vm = this;
-      let sum = 0;
-      vm.cart.forEach(e => {
-        sum += e.price * e.qty
-      });
-      console.log('SUM:' + sum , 'vm.discount:' + vm.discount)
-      vm.finalTotal = sum;
-      vm.total = sum;
-      console.log('vm.total:' + vm.total , 'vm.discount:' + vm.discount)
     }
   },
-  computed:{
-    codeValidate(){
-      const vm = this
-      let message = ''
-      let today = new Date()
-      let someday = new Date()
-      let input = vm.coupon.filter(e => {
-        // someday.setFullYear(e.expiry)
-        if( vm.coupon_code.trim() === '' ){
-          message = '請輸入優惠券'
-          console.log(e.code)
-        }else if( vm.coupon_code.trim() === e.code ){
-          message = '可使用優惠券' +' '+ e.title
-          console.log(e.code)
-        }else{
-          message = '請輸入有效優惠券'
-          console.log(e.code)
+  computed: {
+    codeValidate() {
+      const vm = this;
+      let message = "";
+      vm.coupon.forEach(e => {
+        if (vm.coupon_code.trim() === "" && vm.isCoupon === false) {
+          message = "請輸入優惠券";
+          vm.isWarrning = false;
+        } else if (vm.coupon_code.trim() === e.code) {
+          message = "可使用優惠券" + " " + e.title;
+          vm.isWarrning = false;
+        } else if (vm.isCoupon === true) {
+          message = "購物車商品已經套用優惠券囉";
+          vm.isWarrning = true;
+        } else {
+          message = "請輸入有效優惠券";
+          vm.isWarrning = true;
         }
-      })
-      // console.log(vm.coupon[0].code === 'newmember')
-      return message
+      });
+      return message;
     }
   },
   created() {
     const vm = this;
     vm.storageToCart();
-    // this.addtoCartAPI();
-    // this.getCartAPI();
   }
 };
 </script>
